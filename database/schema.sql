775c42a3ddd08dd7bff4a9feec1ed647857433ae
keith ontiveros <keithontiveros@ucla.edu>
2023-12-10 23:52:58 +0000

    Add tables for likes and comments

--- database/schema.sql
+++ database/schema.sql
@@ -1,16 +1,14 @@
-
 CREATE TABLE users(
-     user_id INT NOT NULL,
+     user_id BIGSERIAL,
      username VARCHAR (20) NOT NULL,
-     name VARCHAR (50),
-     email VARCHAR (40) NOT NULL,
+     profile_name VARCHAR (50),
      date_created TIMESTAMPTZ,
      PRIMARY KEY (user_id)
 );
 
 CREATE TABLE followers (
-     follower INT,
-     followee INT,
+     follower BIGSERIAL,
+     followee BIGSERIAL,
      date_created TIMESTAMPTZ,
      FOREIGN KEY(follower) REFERENCES users(user_id),
      FOREIGN KEY(followee) REFERENCES users(user_id),
@@ -18,8 +16,29 @@ CREATE TABLE followers (
 );
 
 CREATE TABLE posts (
-     user_id INT NOT NULL,
-     content VARCHAR(280),
+     post_id BIGSERIAL,
+     user_id BIGSERIAL,
+     msg VARCHAR(300),
+     likes INT,
      date_created TIMESTAMPTZ,
+     FOREIGN KEY(user_id) REFERENCES users(user_id),
+     PRIMARY KEY (post_id)
+);
+
+CREATE TABLE likes (
+     post_id BIGSERIAL,
+     user_id BIGSERIAL,
+     FOREIGN KEY(post_id) REFERENCES posts(post_id),
+     FOREIGN KEY(user_id) REFERENCES users(user_id),
+     PRIMARY KEY(post_id, user_id)
+);
+
+CREATE TABLE comments (
+     comment_id BIGSERIAL,
+     post_id BIGSERIAL,
+     user_id INT NOT NULL,
+     msg VARCHAR(300),
+     date_created TIMESTAMPTZ,
+     FOREIGN KEY(post_id) REFERENCES posts(post_id),
      FOREIGN KEY(user_id) REFERENCES users(user_id)
-)
+);
