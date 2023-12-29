fd1fc8c236ba7a222378aa338c2b0223f7b95f4b
keith ontiveros <keithontiveros@ucla.edu>
2023-12-29 00:06:12 +0000

    Add public subnet output

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -2,9 +2,9 @@ output "vpc_id" {
   value = aws_vpc.vpc.id
 }
 
-# output "public_subnets" {
-#   value = aws_subnet.public.*.id
-# }
+output "public_subnets" {
+  value = aws_subnet.public.*.id
+}
 
 output "private_subnets" {
   value = aws_subnet.private.*.id
