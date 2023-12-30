4d8bddb0e3581ed0a6141a4b73ba88fed81d9d7d
keith ontiveros <keithontiveros@ucla.edu>
2023-12-30 21:14:40 +0000

    Add api mapping for custom domain

--- terraform/modules/api/custom_domain.tf
+++ terraform/modules/api/custom_domain.tf
@@ -1,16 +1,27 @@
-resource "aws_api_gateway_domain_name" "this" {
-  certificate_arn = var.certificate_arn
-  domain_name     = var.domain
+resource "aws_apigatewayv2_domain_name" "this" {
+  domain_name = var.domain
+
+  domain_name_configuration {
+    certificate_arn = var.certificate_arn
+    endpoint_type   = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
 }
 
 resource "aws_route53_record" "this" {
-  name    = aws_api_gateway_domain_name.this.domain_name
+  name    = aws_apigatewayv2_domain_name.this.domain_name
   type    = "A"
   zone_id = var.route_53_hosted_zone_id
 
   alias {
-    evaluate_target_health = true
-    name                   = aws_api_gateway_domain_name.this.cloudfront_domain_name
-    zone_id                = aws_api_gateway_domain_name.this.cloudfront_zone_id
+    name                   = aws_apigatewayv2_domain_name.this.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.this.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
   }
+}
+
+resource "aws_apigatewayv2_api_mapping" "default" {
+  api_id      = aws_apigatewayv2_api.this.id
+  domain_name = aws_apigatewayv2_domain_name.this.id
+  stage       = aws_apigatewayv2_stage.default.id
 }
