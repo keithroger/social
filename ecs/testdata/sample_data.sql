02e397f00bde29c21961b9242bf11289b91f635e
keith ontiveros <keithontiveros@ucla.edu>
2024-01-03 22:51:17 +0000

    Add sql sample data for running tests

--- ecs/testdata/sample_data.sql
+++ ecs/testdata/sample_data.sql
@@ -1,4 +1,51 @@
-INSERT INTO users(username, profile_name)
-VALUES ('johnathan123', 'jonathan johnson'),
-('alice3', 'alice alexis'),
-('smith', 'Elizabeth Smith')
+-- Users
+INSERT INTO
+    users(username, profile_name)
+VALUES
+    ('johnathan123', 'jonathan johnson'),
+    ('alice3', 'alice alexis'),
+    ('smith', 'Elizabeth Smith'),
+    ('molly1234', 'Molly Monday'),
+    ('123sarah', 'Sarrah Samson'),
+    ('321Josh1', 'Joshua Jackobs'),
+    ('1Jjake', 'Jake Jackson'),
+    ('theChris', 'Chistopher Charlie'),
+    ('elCharlie', 'Charlie Carson'),
+    ('itsDanny', 'Daniella Daniels'),
+    ('bran', 'Brandon Branson'),
+    ('brandy', 'Brandy Bronson'),
+    ('robinson', 'Robin Robinson'),
+    ('Ashley', 'Ashley Ace'),
+    ('itsRach', 'Rachel Randomx'),
+    ('somebody', 'Joe Jameson'),
+    ('aPerson', 'Jen Jetson'),
+    ('laurenL', 'Lauren Long'),
+    ('jazzy', 'Jasmine Le'),
+    ('Hannah', 'Hannah Nguyen'),
+    ('joseRod', 'Jose Rodriquez');
+
+-- Followers
+INSERT INTO
+    followers(follower_id, followee_id)
+VALUES
+    (1, 3),
+    (1, 4),
+    (2, 5),
+    (2, 6),
+    (2, 7),
+    (2, 8),
+    (3, 1),
+    (3, 2),
+    (3, 4),
+    (4, 1),
+    (4, 2),
+    (4, 3),
+    (4, 5),
+    (4, 6),
+    (4, 7),
+    (4, 8),
+    (4, 9),
+    (4, 10),
+    (4, 11),
+    (4, 12),
+    (4, 13);
